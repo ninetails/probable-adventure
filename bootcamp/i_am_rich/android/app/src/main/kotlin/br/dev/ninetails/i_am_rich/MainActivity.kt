fcbb645cf7371826ab7ac55fe62c23925244b8f5
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-07-28 23:33:23 -0300

    feat: 🎸 lecture 26

---
+++ bootcamp/i_am_rich/android/app/src/main/kotlin/br/dev/ninetails/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.dev.ninetails.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bootcamp/i_am_rich/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i.ytimg.com/vi/QH2-TGUlwu4/sddefault.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}

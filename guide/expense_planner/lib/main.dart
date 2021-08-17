591eeaec5b63d875d97f40b496b575f89836c7f6
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-17 16:20:36 -0300

    feat: 🎸 lecture 95

--- guide/expense_planner/lib/main.dart
+++ guide/expense_planner/lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personal Expenses',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );

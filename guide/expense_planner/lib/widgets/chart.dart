607c0608d0a250a690de8ed824e8b120bc2bd30b
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-17 18:00:44 -0300

    feat: 🎸 lecture 102

--- guide/expense_planner/lib/widgets/chart.dart
+++ guide/expense_planner/lib/widgets/chart.dart
@@ -21,7 +21,7 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
@@ -33,7 +33,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: this.groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }

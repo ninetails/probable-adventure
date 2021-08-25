695dc729900fbc0548226592f4f0e1049c5fb842
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-25 09:05:34 -0300

    feat: 🎸 lecture 158

--- guide/delimeals/lib/categories_screen.dart
+++ guide/delimeals/lib/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -6,7 +8,13 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(
+                category.title,
+                category.color,
+                key: ValueKey(category.id),
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,

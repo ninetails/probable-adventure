6044d5a9958717389a56b0085e3e148671439052
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-26 11:49:56 -0300

    feat: 🎸 lecture 209

---
+++ guide/shop/lib/widgets/order_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart' as OrdersProvider;
+
+class OrderItem extends StatelessWidget {
+  final OrdersProvider.OrderItem order;
+
+  const OrderItem({
+    required this.order,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${this.order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

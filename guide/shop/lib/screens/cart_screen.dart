3e736606092b0f6e0c9733df149f4273078b0929
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-26 11:39:35 -0300

    feat: 🎸 lecture 208

--- guide/shop/lib/screens/cart_screen.dart
+++ guide/shop/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart' show Orders;
 // import '../widgets/cart_item.dart' as UI;
 import '../widgets/cart_item.dart';
 
@@ -48,7 +49,11 @@ class CartScreen extends StatelessWidget {
                   ),
                   TextButton(
                     child: Text('Order now'.toUpperCase()),
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(), cart.totalAmount);
+                      cart.clear();
+                    },
                   )
                 ],
               ),

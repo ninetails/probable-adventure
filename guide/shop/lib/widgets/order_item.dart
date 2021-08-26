b058ebe98f80ccf47697a566f77b8c7b7620f012
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2021-08-26 12:11:32 -0300

    feat: 🎸 lecture 211

--- guide/shop/lib/widgets/order_item.dart
+++ guide/shop/lib/widgets/order_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as OrdersProvider;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final OrdersProvider.OrderItem order;
 
   const OrderItem({
@@ -11,21 +13,62 @@ class OrderItem extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${this.order.amount}'),
+            title: Text('\$${this.widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon:
+                  Icon(this._expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () => setState(() => this._expanded = !this._expanded),
             ),
           ),
+          if (this._expanded)
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(widget.order.products.length * 20 + 20, 100),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (cartItem) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            cartItem.product.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${cartItem.quantity}x \$${cartItem.product.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
